3a1f29e5eb14cd1436966852523d164833c81cf7
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-04-16 08:56:13 +0600

    Length_ofLastWord

---
+++ 58_Length_ofLastWord.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        if(s.length() == 0){
+    return 0;
+        }
+        String[] arr = s.split(" ");
+        return arr[arr.length - 1].length();
+        
+    }
+}
