8ef389f2f0c02f1cd2fa49787736a4ed5bb3dfe5
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-07-16 07:15:33 +0600

    InterViewQ

---
+++ Implement strStr().java
@@ -0,0 +1,20 @@
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty())
+            return 0;
+        int m = haystack.length();
+        int n = needle.length();
+        
+        if(m < n)
+            return -1;
+        for(int i = 0; i <= m-n; ++i){
+                int j;
+            for(j = 0; j < n; ++j){
+                if(haystack.charAt(i+j)  != needle.charAt(j))
+                    break;
+            }
+            if(n == j)
+                return i;
+        }
+        return -1;
+    }
+}
